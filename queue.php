a69151cf7c81ff6c1015e32f7d5244ed2d8a1715
Adrian Statescu <mergesortv@gmail.com>
2012-06-30 10:12:28 -0700

    updated queue

--- queue.php
+++ queue.php
@@ -19,12 +19,34 @@
            $xml = $dom->saveXML();
 
            $sql = "INSERT INTO `queue`(func, args) VALUES('$func','$xml')";
-           $obj->query($sql); 
+           $obj->query( $sql ); 
      }
 
      function run_queue() {
            global $obj;
+           $sql = "select * from `queue`";
+           $obj->query( $sql ); 
+
+           while(($row=$obj->fetch()) !== false)  {
+                  $id = $row['id'];  
+                  $func = $row['func'];
+                  $argsxml = $row['args'];
+
+                  $dom = new DomDocument();
+                  $dom->loadXML($argsxml);
+
+                  $args = array();
+
+                  foreach($dom->getElementsByTagName("argument") as $node) {
+
+                          $args[] = $node->nodeValue;
+                  }  
+echo"<pre>";
+print_r($args);
+echo"</pre>";
+                  //call_user_func_array($func, args);           
+           }
     }
 
-    add_to_queue("mail",array("adrian","statescu")); 
+    run_queue(); 
 ?>
